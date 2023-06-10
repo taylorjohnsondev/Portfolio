fc0a100edbf06b473ac00654b917ae3e5fda0d6b
Taylor johnson <taylorjohnsondev@gmail.com>
2023-06-10 11:49:45 -0400

    updated css

--- client/src/Components/Infoboxes.js
+++ client/src/Components/Infoboxes.js
@@ -1,7 +1,7 @@
 import "../Components/CSS/infobox.css";
-import { DiReact } from "react-icons/di";
-import { SiHtml5, SiMongodb, SiExpress } from "react-icons/si";
-import { FaCss3Alt } from "react-icons/fa";
+import { DiReact, DiJavascript, DiHtml5 } from "react-icons/di";
+import {  SiMongodb, SiExpress } from "react-icons/si";
+import { FaCss3Alt, FaAws, FaBootstrap } from "react-icons/fa";
 import { IoLogoNodejs } from "react-icons/io";
 
 const Infobox = () => {
@@ -9,29 +9,41 @@ const Infobox = () => {
     <section id="Projects">
       <h1>My Skills</h1>
       <div className="infoboxes-container">
-        <article className="portfolio-item">
+        <article className="infobox-item">
           <DiReact />
           <h3>React</h3>
         </article>
-        <article className="portfolio-item">
-          <SiMongodb />
-          <h3>MongoDB</h3>
+        <article className="infobox-item">
+          <DiJavascript />
+          <h3>JavaScript</h3>
         </article>
-        <article className="portfolio-item">
-          <IoLogoNodejs />
+        <article className="infobox-item">
+          <IoLogoNodejs/>
           <h3>Node.js</h3>
         </article>
-        <article className="portfolio-item">
+        <article className="infobox-item">
           <SiExpress/>
           <h3>Express</h3>
         </article>
-        <article className="portfolio-item">
+        <article className="infobox-item">
+          <DiHtml5 />
+          <h3>HTML 5</h3>
+        </article>
+        <article className="infobox-item">
+          <FaAws />
+          <h3>AWS</h3>
+        </article>
+        <article className="infobox-item">
+          <FaBootstrap />
+          <h3>Bootstrap</h3>
+        </article>
+        <article className="infobox-item">
           <FaCss3Alt />
           <h3>CSS</h3>
         </article>
-        <article className="portfolio-item">
-          <SiHtml5 />
-          <h3>HTML 5</h3>
+        <article className="infobox-item">
+          <SiMongodb />
+          <h3>MongoDB</h3>
         </article>
       </div>
     </section>
