f4e55bad162a3ce6e32fa887c2c465d5e374da40
Taylor johnson <taylorjohnsondev@gmail.com>
2023-07-13 18:30:59 -0400

    updated resume

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -3,8 +3,14 @@ import "../Components/CSS/resume.css";
 const Resume = () => {
   return (
     <section>
-      <img id="resume" src="resume1.jpg" alt="Taylors Resume" />
-      <img id="resume" src="resume2.jpg" alt="Taylors Resume" />
+      <img id="resume" src="resume1.png" alt="Taylors Resume" />
+      <img id="resume" src="resume2.png" alt="Taylors Resume" />
+      <a
+          href="https://docs.google.com/document/d/1rwTK9PQ0PrmdLoD4uJWRWNaI4_npHHAhxYrLUYgPsbQ/edit?usp=sharing"
+          className="btn" 
+        >
+          View on Google Docs
+        </a>
     </section>
   );
 };
